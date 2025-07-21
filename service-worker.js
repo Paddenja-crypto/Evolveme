d12c98be4b78ff4c11d81f172207703824e25e66
Paddenja-crypto <Paddenja@hotmail.com>
2025-07-22 11:13:03 +1200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,12 @@
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('evolveme').then(function(cache) {
+    caches.open('evolveme-v2').then(function(cache) {
       return cache.addAll([
         './index.html',
         './manifest.json',
-        './icon.png'
+        './stage0.png',
+        './stage1.png',
+        './stage2.png'
       ]);
     })
   );
